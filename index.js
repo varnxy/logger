41ce4a21c9b643a2ba2f43590bec2b3843c58e20
Yusril Herlian Syah <yusrilhsyah@gmail.com>
2019-02-13 09:00:49 +0700

    Change writer and format

--- index.js
+++ index.js
@@ -1,29 +1,39 @@
 const mkdirp = require('mkdirp')
     , path = require('path')
     , vsprintf = require('sprintf-js').vsprintf
+    , sprintf = require('sprintf-js').sprintf
     , timestamp = require('time-stamp')
     , chalk = require('chalk')
-    , rtrim = require('rtrim')
+    , caller = require('caller')
+    , ltrim = require('ltrim')
+    , timestampFormat = 'HH:mm:ss'
 
 let logDirectory = ''
 
-function Logger(name) {
+function Logger(name, basefile) {
   this.name = name || ''
   this.defaultArgsFormat = []
   this.logTypes = ['info', 'error', 'warn']
   this.colorMap = {
     info: 'blue',
     error: 'red',
-    warn: 'yellow'
+    warn: 'yellow',
+    debug: 'cyan'
   }
+  this.writer = {
+    info: 'stdout',
+    error: 'stderr',
+    warn: 'stderr',
+    debug: 'stdout'
+  }
+  this.basedir = path.dirname(basefile)
 
   this.setup()
 }
 
 Logger.prototype.setup = function() {
   if (logDirectory) {
-    logDirectory = path.resolve(path.dirname(__filename), logDirectory)
-    console.log(logDirectory)
+    logDirectory = path.resolve(this.basedir, logDirectory)
 
     mkdirp.sync(logDirectory)
     this._createRotateLogger()
@@ -41,49 +51,57 @@ Logger.prototype._createRotateLogger = function() {
   })
 
   this.logTypes.forEach(msgType => {
-    this[msgType] = (msg) => {
-      let fmtArgs = this._createFormatArgs(msgType, msg)
+    this[msgType] = function() {
+      let msg = sprintf.apply(null, arguments)
+        , fmtArgs = this._createFormatArgs(msgType, msg)
 
       logStream.write(vsprintf(fmtArgs.format, fmtArgs.args))
-    }
+    }.bind(this)
   })
 }
 
 Logger.prototype._createChalkLogger = function() {
   this.logTypes.forEach(msgType => {
-    this[msgType] = (msg) => {
-      let fmtArgs = this._createFormatArgs(msgType, msg)
+    this[msgType] = function() {
+      let msg = sprintf.apply(null, arguments)
+        , fmtArgs = this._createFormatArgs(msgType, msg)
 
       fmtArgs.args[0] = chalk.green(fmtArgs.args[0])
       fmtArgs.args[1] = chalk[this.colorMap[msgType]](fmtArgs.args[1])
 
-      if (fmtArgs.args.length > 3) {
-        fmtArgs.args[2] = chalk.magenta(fmtArgs.args[2])
-        fmtArgs.args[3] = rtrim(fmtArgs.args[3], '\r\n')
-      } else {
-        fmtArgs.args[2] = rtrim(fmtArgs.args[2], '\r\n')
-      }
+      fmtArgs.args[2] = chalk.magenta(fmtArgs.args[2])
 
-      console.log(vsprintf(fmtArgs.format, fmtArgs.args))
-    }
+      let output = vsprintf(fmtArgs.format, fmtArgs.args)
+
+      process[this.writer[msgType]].write(output)
+    }.bind(this)
   })
 }
 
 Logger.prototype._createFormatArgs = function(msgType, msg) {
+  let logName = this.name
+
+  if (!logName) {
+    logName = caller(2)
+    // Simplified path
+    logName = ltrim(logName.replace(this.basedir, ''), path.sep)
+  }
+
   msg = (msg + '') + '\r\n'
-  let fmt = (this.name) ? '[%s:%s:%s] %s' : '[%s:%s] %s'
-    , args = (this.name)
-    ? [timestamp('HH:mm:ss'), msgType.toUpperCase(), this.name, msg]
-    : [timestamp('HH:mm:ss'), msgType.toUpperCase(), msg]
 
   return {
-    format: fmt,
-    args: args
+    format: '[%s:%s:%s] %s',
+    args: [
+      timestamp(timestampFormat),
+      msgType.toUpperCase(),
+      logName,
+      msg
+    ]
   }
 }
 
 function loggerFactory(name) {
-  return new Logger(name)
+  return new Logger(name, caller())
 }
 
 loggerFactory.setDirectory = function(logDir) {
